daf038faaa8325270d468c136b6e7f5f97b7e99a
DanielMacura <danny.macura@gmail.com>
2023-09-07 13:47:59 +0200

    Add Parcel

---
+++ dist/index.b17dd32f.js
@@ -0,0 +1,15 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event)=>{
+    const progressBar = event.target.querySelector(".progress-bar");
+    const updatingBar = event.target.querySelector(".update-bar");
+    if (event.detail.totalProgress === 0) {
+        progressBar.style.display = "block";
+        updatingBar.style.width = "0%";
+    } else {
+        updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+        if (event.detail.totalProgress === 1) progressBar.style.display = "none";
+    }
+};
+document.querySelector("model-viewer").addEventListener("progress", onProgress);
+
+//# sourceMappingURL=index.b17dd32f.js.map
